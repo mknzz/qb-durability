3e230639ae36317821aac4f433c0dab56557c8e3
mknzz <85302866+mknzz@users.noreply.github.com>
2022-01-01 23:17:36 +0000

    fixes
    fix items not deleting
    fix missing items

--- server/main.lua
+++ server/main.lua
@@ -6,29 +6,26 @@ AddEventHandler("qb-durability:server:ItemQuality", function(data, hp)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-    local removing = false
     for i=0, 41 do
         local itemSlot = Player.Functions.GetItemBySlot(i)
         if itemSlot ~= nil then
-            if Player.PlayerData.items[i].info.quality == nil and not removing then
-                Player.PlayerData.items[i].info.quality = 100
-                Player.Functions.SetInventory(Player.PlayerData.items)
-            end
-
             local amount = Config.DurabilityAmount["default"]
             if Config.DurabilityAmount[itemSlot.name] ~= nil then
                 amount = Config.DurabilityAmount[itemSlot.name]
             end
 
-            Player.PlayerData.items[i].info.quality = Player.PlayerData.items[i].info.quality - amount
-            Player.Functions.SetInventory(Player.PlayerData.items)
-            print(itemSlot.name.." removed "..amount.." durability")
-
-            if Player.PlayerData.items[i].info.quality <= 0.0 or Player.PlayerData.items[i].info.quality <= 0 then
-                Player.Functions.RemoveItem(itemSlot.name, 1)
-                removing = true
-                print(itemSlot.name.." removed ")
+            if Player.PlayerData.items[i].info.quality == nil then
+                Player.PlayerData.items[i].info.quality = 100
+            else
+                Player.PlayerData.items[i].info.quality = Player.PlayerData.items[i].info.quality - amount
+                print(itemSlot.name.." removed "..amount.." durability")
+                if Player.PlayerData.items[i].info.quality <= 0.0 or Player.PlayerData.items[i].info.quality <= 0 then
+                    Player.Functions.RemoveItem(itemSlot.name, 1)
+                    print(itemSlot.name.." removed ")
+                    return
+                end
             end
+            Player.Functions.SetInventory(Player.PlayerData.items)
         end
     end
 end)
